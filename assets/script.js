6fb432319500ee6b7f3c20335d8c79c2d97609b8
Anthony Castillo <anthonycastillo821@gmail.com>
2021-03-26 13:39:27 -0500

    adding file structure

---
+++ assets/script.js
@@ -0,0 +1,29 @@
+let city = "dallas"
+let defaultAPI = " "
+
+
+
+fetch("api.openweathermap.org/data/2.5/forecast?q=dallas&appid= ")
+
+.then(response => response.json())
+.then(function(data){
+
+console.log(data);
+
+})
+fetch("api.openweathermap.org/data/2.5/forecast?q=dallas&appid= ")
+
+.then(response => response.json())
+.then(function(data){
+
+console.log(data);
+
+})
+fetch("api.openweathermap.org/data/2.5/forecast?q=dallas&appid= ")
+
+.then(response => response.json())
+.then(function(data){
+
+console.log(data);
+
+})
